1bd4e9c1687eda6fbf1094df9681dee3d0b40576
rahulworld <rahulnitsxr@gmail.com>
2018-06-11 23:32:59 +0530

    added

--- istsos/actions/retrievers/aiopg/unitConversion.py
+++ istsos/actions/retrievers/aiopg/unitConversion.py
@@ -7,25 +7,25 @@ import asyncio
 from istsos.actions.retrievers.unitConversion import UnitConversion
 
 
-class UnitConversion(UnitConversion):
-    @asyncio.coroutine
-    def process(self, request):
-        with (yield from request['state'].pool.cursor()) as cur:
-            sql = """
-                SELECT rainfall, humidity, temperature, velocity
-                FROM obs;
-            """
-            yield from cur.execute(sql)
-            request['unit_conversion'] = []
-            recs = yield from cur.fetchall()
-            for rec in recs:
-                request['unit_conversion'].append({
-                    "rainfall": rec[0],
-                    "humidity": rec[1],
-                    "temperature": rec[2],
-                    "velocity": rec[3]
-                })
-        print("aipog Unit Conversion in Database Query")
+# class UnitConversion(UnitConversion):
+#     @asyncio.coroutine
+#     def process(self, request):
+#         with (yield from request['state'].pool.cursor()) as cur:
+#             sql = """
+#                 SELECT rainfall, humidity, temperature, velocity
+#                 FROM obs;
+#             """
+#             yield from cur.execute(sql)
+#             request['unit_conversion'] = []
+#             recs = yield from cur.fetchall()
+#             for rec in recs:
+#                 request['unit_conversion'].append({
+#                     "rainfall": rec[0],
+#                     "humidity": rec[1],
+#                     "temperature": rec[2],
+#                     "velocity": rec[3]
+#                 })
+#         print("aipog Unit Conversion in Database Query")
 
 
 # class UnitConversion(UnitConversion):
@@ -41,10 +41,35 @@ class UnitConversion(UnitConversion):
 #             recs = yield from cur.fetchall()
 #             for rec in recs:
 #                 request['unit_conversion'].append({
-#                     "timestamp": rec[0],
-#                     "rainfall": rec[1],
-#                     "humidity": rec[2],
-#                     "temperature": rec[3],
-#                     "velocity": rec[4]
+#                     "timestamp": str(rec[0]),
+#                     "rainfall": float(rec[1]),
+#                     "humidity": float(rec[2]),
+#                     "temperature": float(rec[3]),
+#                     "velocity": float(rec[4])
 #                 })
 #         print("aipog Unit Conversion in Database Query")
+
+class UnitConversion(UnitConversion):
+    @asyncio.coroutine
+    def process(self, request):
+        with (yield from request['state'].pool.cursor()) as cur:
+            from_unit=request['json']['from']
+            to_unit=request['json']['to']
+            sql1="""CREATE EXTENSION IF NOT EXISTS unit;"""
+            yield from cur.execute(sql1)
+            sql = """
+                SELECT iso8601, rainfall, humidity*'"""+from_unit+"""'::unit@ '"""+to_unit+"""', temperature, velocity
+                FROM obs;
+            """
+            yield from cur.execute(sql)
+            request['unit_conversion'] = []
+            recs = yield from cur.fetchall()
+            for rec in recs:
+                request['unit_conversion'].append({
+                    "timestamp": str(rec[0]),
+                    "rainfall": float(rec[1]),
+                    "humidity": rec[2],
+                    "temperature": float(rec[3]),
+                    "velocity": float(rec[4])
+                })
+        print("aipog Unit Conversion in Database Query")
--- istsos/actions/servers/rest/units/unitConversion.py
+++ istsos/actions/servers/rest/units/unitConversion.py
@@ -12,79 +12,79 @@ import psycopg2
 from configparser import ConfigParser
 import json
 
-def connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        db = {}
-        db["host"]="localhost"
-        db["database"]="ist3"
-        db["user"]="postgres"
-        db["password"]="postgres"
-        # read connection parameters
-        params = db
+# def connect():
+#     """ Connect to the PostgreSQL database server """
+#     conn = None
+#     try:
+#         db = {}
+#         db["host"]="localhost"
+#         db["database"]="ist3"
+#         db["user"]="postgres"
+#         db["password"]="postgres"
+#         # read connection parameters
+#         params = db
  
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
+#         # connect to the PostgreSQL server
+#         print('Connecting to the PostgreSQL database...')
+#         conn = psycopg2.connect(**params)
  
-        # create a cursor
-        cur = conn.cursor()
+#         # create a cursor
+#         cur = conn.cursor()
         
- # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
+#  # execute a statement
+#         print('PostgreSQL database version:')
+#         cur.execute('SELECT version()')
  
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
+#         # display the PostgreSQL database server version
+#         db_version = cur.fetchone()
+#         print(db_version)
        
-     # close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
+#      # close the communication with the PostgreSQL
+#         cur.close()
+#     except (Exception, psycopg2.DatabaseError) as error:
+#         print(error)
+#     finally:
+#         if conn is not None:
+#             conn.close()
+#             print('Database connection closed.')
 
 
-def get_parts(from_unit,to_unit):
-    """ query parts from the parts table """
-    conn = None
-    try:
-        db = {}
-        db["host"]="localhost"
-        db["database"]="ist3"
-        db["user"]="postgres"
-        db["password"]="postgres"
-        # read connection parameters
-        params = db
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        # cur.execute("SELECT open*'MB/min'::unit@ 'GB/d',close*'m'::unit@ 'km' from bitprice")
-        # cur.execute("SELECT array_to_json(array_agg(humidity*'m'::unit@ 'mi')) from obs")
-        # cur.execute("SELECT array_to_json(array_agg(obs)) from obs")
-        # cur.execute("SELECT array_to_json(array_agg(row(iso8601, humidity))) from obs")
-        cur.execute("select row_to_json(t) from (select iso8601, humidity*'"+from_unit+"'::unit@ '"+to_unit+"' from obs) t")
-        rows = cur.fetchall()
-        # print("The number of parts: ", cur.rowcount)
-        # for row in rows:
-        #     print(row)
-        # print(rows[0])
-        # print(type(rows[0]))
-        # print(type(rows[0][0]))
-        # print(rows[0][0])
-        # print("apoov")
-        # data_ch = dict(rows[0])
-        # print(data_ch)
-        cur.close()
-        return rows
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+# def get_parts(from_unit,to_unit):
+#     """ query parts from the parts table """
+#     conn = None
+#     try:
+#         db = {}
+#         db["host"]="localhost"
+#         db["database"]="ist3"
+#         db["user"]="postgres"
+#         db["password"]="postgres"
+#         # read connection parameters
+#         params = db
+#         conn = psycopg2.connect(**params)
+#         cur = conn.cursor()
+#         # cur.execute("SELECT open*'MB/min'::unit@ 'GB/d',close*'m'::unit@ 'km' from bitprice")
+#         # cur.execute("SELECT array_to_json(array_agg(humidity*'m'::unit@ 'mi')) from obs")
+#         # cur.execute("SELECT array_to_json(array_agg(obs)) from obs")
+#         # cur.execute("SELECT array_to_json(array_agg(row(iso8601, humidity))) from obs")
+#         cur.execute("select row_to_json(t) from (select iso8601, humidity*'"+from_unit+"'::unit@ '"+to_unit+"' from obs) t")
+#         rows = cur.fetchall()
+#         # print("The number of parts: ", cur.rowcount)
+#         # for row in rows:
+#         #     print(row)
+#         # print(rows[0])
+#         # print(type(rows[0]))
+#         # print(type(rows[0][0]))
+#         # print(rows[0][0])
+#         # print("apoov")
+#         # data_ch = dict(rows[0])
+#         # print(data_ch)
+#         cur.close()
+#         return rows
+#     except (Exception, psycopg2.DatabaseError) as error:
+#         print(error)
+#     finally:
+#         if conn is not None:
+#             conn.close()
 
 
 class UnitConversion(CompositeAction):
@@ -98,33 +98,10 @@ class UnitConversion(CompositeAction):
 
     @asyncio.coroutine
     def after(self, request):
-        from_unit=request['json']['from']
-        to_unit=request['json']['to']
-        # print("unit conversion from - to")
-        # print(from_unit+to_unit)
-        print("unit conversion ######2")
-        print(request)
-        connect()
-        rows = get_parts(from_unit,to_unit)
-        # row=rows[0]
-        all_data = []
-        for i in rows:
-            tu = i[0]
-            all_data.append(tu)
-        data1=[json.dumps(all_data[0])]
-        # print(get_parts())
-        # print(rows)
-            # print(tu)
-            # print("*"*20)
-        # final_dict = get_parts()
-        # all_keys = final_dict.keys()
-        # new_dict = dict()
-        # for key in all_keys:
-        #     new_dict[key.strip()] = final_dict[key].strip()
-
+        print(request['unit_conversion'])
         request['response'] = Response(
             Response.get_template({
-                "data": request['unit_conversion']
+                "data": request['unit_conversion'][1]
                 # "data" : data1
                 # "data": request['uoms']
             })
