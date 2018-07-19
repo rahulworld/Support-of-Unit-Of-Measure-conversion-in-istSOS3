c6714249162b86fa1313428b165c3fc98005baf7
rahulworld <rahulnitsxr@gmail.com>
2018-07-19 23:51:21 +0530

    adding lookup table

---
+++ istsos/plugins/unit_con_post/lookUpTable.py
@@ -0,0 +1,7 @@
+lookups = {
+    "°C":["°C", "celcius", "degree Celsius", "degree-celsius", "degree-celsius", "degree", "degree centigrade", "degcelsius", "degC"],
+    "°F":["°F", "fahrenheit", "degfahrenheit", "degF", "degfahrenheit", "degree-fahrenheit", "degree fahrenheit"],
+    "°K":["°K", "kelvin", "degK", "tempK"],
+    "°K":["°K", "kelvin", "degK", "tempK"],
+    "°K":["°K", "kelvin", "degK", "tempK"],
+     }
